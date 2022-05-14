ac7263b8b012d20ec62a0a0164eb44b21a2180a3
eddym <edwinmuthee11@gmail.com>
2022-05-14 19:28:54 +0300

    try catch updates

--- bank_account_simulation.py
+++ bank_account_simulation.py
@@ -12,173 +12,191 @@ class Accounts:
         print("Welcome to our banking system")
     #deposit function
     def deposit(self):
-        # check who is making the deposit
-        if self.name == "wanjiru":
-            amount = float(input("Enter amount to be Deposited: "))
+        try:
+            # check who is making the deposit
+            if self.name == "wanjiru":
+                amount = float(input("Enter amount to be Deposited: "))
 
-            # Credit wanjiru's account
-            self.Wanjiru_account += amount
-            print("\n Amount Deposited:\t", amount)
+                # Credit wanjiru's account
+                self.Wanjiru_account += amount
+                print("\n Amount Deposited:\t", amount)
 
-        # check who is making the deposit
-        elif self.name == "juma":
-            amount = float(input("Enter amount to be Deposited:\t"))
+            # check who is making the deposit
+            elif self.name == "juma":
+                amount = float(input("Enter amount to be Deposited:\t"))
 
-            # Credit Juma's account
-            self.Juma_account += amount
-            print("\n Amount Deposited:\t", amount)
+                # Credit Juma's account
+                self.Juma_account += amount
+                print("\n Amount Deposited:\t", amount)
 
-        # check who is making the deposit
-        else:
-            amount = float(input("Enter amount to be Deposited:\t"))
+            # check who is making the deposit
+            else:
+                amount = float(input("Enter amount to be Deposited:\t"))
 
-            # Credit Linda's account
-            self.Linda_account += amount
-            print("\n Amount Deposited:\t", amount)
+                # Credit Linda's account
+                self.Linda_account += amount
+                print("\n Amount Deposited:\t", amount)
+        except:
+            print("Something went wrong")
 
     """ check if the user has any amount in their accounts and verify who is doing the withdrawal before making the withdrawal
             from their account and the return insufficient message if account is less"""
     def withdraw(self):
+        try:
+            if self.name == "wanjiru":
+                amount = float(input("Enter amount to be Withdrawn:\t"))
+                if self.Wanjiru_account >= amount:
+                    self.Wanjiru_account -= amount
+                    print("\n You Withdrew:\t", amount)
+                else:
+                    print("\n Insufficient balance")
 
-        if self.name == "wanjiru":
-            amount = float(input("Enter amount to be Withdrawn:\t"))
-            if self.Wanjiru_account >= amount:
-                self.Wanjiru_account -= amount
-                print("\n You Withdrew:\t", amount)
-            else:
-                print("\n Insufficient balance")
-
-        elif self.name == "juma":
-            amount = float(input("Enter amount to be Withdrawn:\t"))
-            if self.Juma_account >= amount:
-                self.Juma_account -= amount
-                print("\n You Withdrew:\t", amount)
-            else:
-                print("\n Insufficient balance")
-
-        else:
-            amount = float(input("Enter amount to be Withdrawn:\t"))
-
-            if self.Linda_account >= amount:
-                self.Linda_account -= amount
-                print("\n You Withdrew:\t", amount)
+            elif self.name == "juma":
+                amount = float(input("Enter amount to be Withdrawn:\t"))
+                if self.Juma_account >= amount:
+                    self.Juma_account -= amount
+                    print("\n You Withdrew:\t", amount)
+                else:
+                    print("\n Insufficient balance")
 
             else:
-                print("\n Insufficient balance")
+                amount = float(input("Enter amount to be Withdrawn:\t"))
+
+                if self.Linda_account >= amount:
+                    self.Linda_account -= amount
+                    print("\n You Withdrew:\t", amount)
+
+                else:
+                    print("\n Insufficient balance")
+        except:
+            print("Something went wrong")
 
     """Bank transfer for customers depending on the account balance"""
     def transfer(self):
-        if self.name == "wanjiru":
-            amount = float(input("Enter amount to be transfer:\t"))
+        try:
+            if self.name == "wanjiru":
+                amount = float(input("Enter amount to be transfer:\t"))
 
-            transferee= str(input("enter user to send to, Linda or Juma\t")).lower()
+                transferee= str(input("enter user to send to, Linda or Juma\t")).lower()
 
-            if self.Wanjiru_account >= amount:
-                self.Wanjiru_account -= amount
+                if self.Wanjiru_account >= amount:
+                    self.Wanjiru_account -= amount
 
-                if transferee == "juma":
-                    self.Juma_account += amount
+                    if transferee == "juma":
+                        self.Juma_account += amount
 
-                elif transferee == "linda":
-                    self.Linda_account += amount
+                    elif transferee == "linda":
+                        self.Linda_account += amount
 
-                print("\n You transferred:\t", amount)
-            else:
-                print("\n Insufficient balance  ")
+                    print("\n You transferred:\t", amount)
+                else:
+                    print("\n Insufficient balance  ")
 
-        elif self.name == "Juma":
-            amount = float(input("Enter amount to be transfer:\t"))
+            elif self.name == "Juma":
+                amount = float(input("Enter amount to be transfer:\t"))
 
-            transferee = str(input("enter user to send to, wanjiru or Linda:\t")).lower()
+                transferee = str(input("enter user to send to, wanjiru or Linda:\t")).lower()
 
-            if self.Juma_account >= amount:
-                self.Juma_account -= amount
+                if self.Juma_account >= amount:
+                    self.Juma_account -= amount
 
-                if transferee == "wanjiru":
-                    self.Wanjiru_account += amount
+                    if transferee == "wanjiru":
+                        self.Wanjiru_account += amount
 
-                elif transferee == "linda":
-                    self.Linda_account += amount
-                print("\n You transferred:\t", amount)
+                    elif transferee == "linda":
+                        self.Linda_account += amount
+                    print("\n You transferred:\t", amount)
 
-            else:
-                print("\n Insufficient balance  ")
-
-        else:
-            amount = float(input("Enter amount to be transfer:\t"))
-
-            transferee = str(input("enter user to send to, wanjiru or Juma:\t")).lower()
-
-            if self.Linda_account >= amount:
-                self.Linda_account -= amount
-
-                if transferee == "juma":
-                    self.Juma_account += amount
-
-                elif transferee == "wanjiru":
-                    self.Wanjiru_account += amount
-
-                print("\n You transferred\t:", amount)
+                else:
+                    print("\n Insufficient balance  ")
 
             else:
-                print("\n Insufficient balance  ")
+                amount = float(input("Enter amount to be transfer:\t"))
 
+                transferee = str(input("enter user to send to, wanjiru or Juma:\t")).lower()
+
+                if self.Linda_account >= amount:
+                    self.Linda_account -= amount
+
+                    if transferee == "juma":
+                        self.Juma_account += amount
+
+                    elif transferee == "wanjiru":
+                        self.Wanjiru_account += amount
+
+                    print("\n You transferred\t:", amount)
+
+                else:
+                    print("\n Insufficient balance  ")
+        except:
+            print("Something went wrong")
 
 
     def display(self):
-        if self.name == "wanjiru":
-            print("\n Net Available Balance=", self.Wanjiru_account)
-        elif self.name == "juma":
-            print("\n Net Available Balance=", self.Juma_account)
-        else:
-            print("\n Net Available Balance=", self.Linda_account)
+        try:
+            if self.name == "wanjiru":
+                print("\n Net Available Balance=", self.Wanjiru_account)
+            elif self.name == "juma":
+                print("\n Net Available Balance=", self.Juma_account)
+            else:
+                print("\n Net Available Balance=", self.Linda_account)
+        except:
+            print("Something went wrong")
+
 
     def credentials(self):
-        while True:
-            self.name= str(input("please enter you name of customer to continue (Wanjiru, Juma, Linda): ")).lower()
-            if self.name == "wanjiru":
-                print("\n your account ballance is:", self.Wanjiru_account)
-                service = int(input("Please select service: 1=Deposit\n 2=Withdraw\n 3=Transfer:  "))
-                if service ==1:
-                    self.deposit()
-                elif service ==2:
-                    self.withdraw()
-                elif service ==3:
-                    self.transfer()
-                else:
-                    print("you have entered wrong value")
-                self.display()
+        try:
+            while True:
+                self.name= str(input("please enter you name of customer to continue (Wanjiru, Juma, Linda): ")).lower()
+                if self.name == "wanjiru":
+                    print("\n your account ballance is:", self.Wanjiru_account)
+                    service = int(input("Please select service:\n 1=Deposit\n 2=Withdraw\n 3=Transfer:\n 4. Check balance\t"))
+                    if service ==1:
+                        self.deposit()
+                    elif service ==2:
+                        self.withdraw()
+                    elif service ==3:
+                        self.transfer()
+                    elif service == 4:
+                        self.display()
+                    else:
+                        print("you have entered wrong process")
 
-            elif self.name == "juma":
-                print("\n your account ballance is:", self.Juma_account)
-                service = int(input("Please select service: 1=Deposit\n 2=Withdraw\n 3=Transfer:  "))
-                if service == 1:
-                    self.deposit()
-                elif service == 2:
-                    self.withdraw()
-                elif service == 3:
-                    self.transfer()
-                else:
-                    print("you have entered wrong value")
+                elif self.name == "juma":
+                    print("\n your account ballance is:", self.Juma_account)
+                    service = int(input("Please select service: 1=Deposit\n 2=Withdraw\n 3=Transfer:\n 4. Check balance\t"))
+                    if service == 1:
+                        self.deposit()
+                    elif service == 2:
+                        self.withdraw()
+                    elif service == 3:
+                        self.transfer()
+                    elif service == 4:
+                        self.display()
+                    else:
+                        print("you have entered wrong process")
 
-                self.display()
+                elif self.name == "linda":
+                    print("\n your account ballance is:", self.Linda_account)
+                    service = int(input("Please select service: 1=Deposit\n 2=Withdraw\n 3=Transfer:\n 4. Check balance\t"))
+                    if service == 1:
+                        self.deposit()
+                    elif service == 2:
+                        self.withdraw()
+                    elif service == 3:
+                        self.transfer()
+                    elif service == 4:
+                        self.display()
+                    else:
+                        print("you have entered wrong process")
 
-            elif self.name == "linda":
-                print("\n your account ballance is:", self.Linda_account)
-                service = int(input("Please select service: 1=Deposit\n 2=Withdraw\n 3=Transfer:  "))
-                if service == 1:
-                    self.deposit()
-                elif service == 2:
-                    self.withdraw()
-                elif service == 3:
-                    self.transfer()
-                else:
-                    print("you have entered wrong value")
-                self.display()
-
-            final_procedure = str(input("are you done, Yes or No\n")).lower()
-            if final_procedure == "yes":
-                break
+                final_procedure = str(input("are you done, Yes or No\n")).lower()
+                if final_procedure == "yes":
+                    break
+        except:
+            print("Something went wrong")
+        else:
+            print("Nothing went wrong")
 
 # Driver code
 
